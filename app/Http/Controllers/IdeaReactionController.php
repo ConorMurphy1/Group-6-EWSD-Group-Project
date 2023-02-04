339e370bfd84eacef2993223d3aabff5c1d1558c
ConorMurphy1 <conoromurichucmurphy@mail.com>
2023-02-04 12:23:37 +0630

    conor

---
+++ app/Http/Controllers/IdeaReactionController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\IdeaReaction;
+use Illuminate\Http\Request;
+
+class IdeaReactionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\IdeaReaction  $ideaReaction
+     * @return \Illuminate\Http\Response
+     */
+    public function show(IdeaReaction $ideaReaction)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\IdeaReaction  $ideaReaction
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(IdeaReaction $ideaReaction)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\IdeaReaction  $ideaReaction
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, IdeaReaction $ideaReaction)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\IdeaReaction  $ideaReaction
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(IdeaReaction $ideaReaction)
+    {
+        //
+    }
+}
---
+++ app/Models/IdeaReaction.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class IdeaReaction extends Model
+{
+    use HasFactory;
+}
