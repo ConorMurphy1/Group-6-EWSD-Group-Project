c090bfc6418e7864885042952f23b797fd2af2e0
Myat Min <myatm.minm@gmail.com>
2023-03-11 20:56:52 +0630

    checking for update

---
+++ app/Http/Controllers/IdeaReportController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class IdeaReportController extends Controller
+{
+    public function chartData(){
+        $idea = Idea::where 
+    }
+}
