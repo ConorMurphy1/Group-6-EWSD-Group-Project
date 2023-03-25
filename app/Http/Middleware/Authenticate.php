207c98904b9951ad911e3b9aec2a448d7a273b63
NyeinChan Soe <nyeinchansoe.business@gmail.com>
2023-03-25 10:02:38 +0630

    Fix auth middleware redirect

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -15,7 +15,7 @@ class Authenticate extends Middleware
     protected function redirectTo($request)
     {
         if (! $request->expectsJson()) {
-            return route('session.login');
+            return route('login');
         }
     }
 }
