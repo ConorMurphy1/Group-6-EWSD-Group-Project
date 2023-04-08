f56c7bc3a3424dcd4835494cd6ac148f3bc0db0b
ConorMurphy1 <conoromurichucmurphy@mail.com>
2023-04-08 23:52:51 +0630

    conor

--- resources/views/role/index.blade.php
+++ resources/views/role/index.blade.php
@@ -18,8 +18,6 @@
                             <tr>
                                 <th>No</th>
                                 <th>Role</th>
-                                <th>Deleted At</th>
-                                <th>Action</th>
                             </tr>
                         </thead>
                         <tbody>
@@ -27,24 +25,6 @@
                             <tr>
                                 <td>{{ ($roles->currentPage()-1) * $roles->perPage() + $loop->index + 1 }}</td>
                                 <td>{{ $idea->role }}</td>
-                                <td>{{ $idea->deleted_at }}</td>
-                                <td class="flex-warp">
-                                    <div class="mx-3 text-center">
-                                        <form action="{{ route('roles.destroy', $idea->id) }}" method="post">
-                                            @csrf @method('delete')
-                                            <a @if (Auth::user()->role->id  != 3)
-                                                onclick="return false;" disabled
-                                            @endif href="{{ route('roles.edit', $idea->id) }}" class="btn btn-info btn-sm" data-toggle="tooltip" data-placement="top" title="Edit">
-                                                <i class="fa fa-edit"></i>
-                                            </a>
-                                            <button @if (Auth::user()->role->id  != 3)
-                                                disabled
-                                            @endif type="submit" class="btn btn-danger btn-sm" onclick="return confirm('Are you sure you want to delete?')" data-toggle="tooltip" data-placement="top" title="Delete">
-                                                <i class="fa fa-trash-o text-dark"></i>
-                                            </button>
-                                        </form>
-                                    </div>
-                                </td>
                             </tr>
                             @endforeach
                         </tbody>
